ca86dd06f9157cadf681bc1f3ae11dcb04b76291
Caue Garcia <cauerg7@outlook.com>
2023-11-06 16:39:02 -0300

    query with multiple criteria

--- src/main/java/com/cauergarcia/workshopmongodb/services/PostService.java
+++ src/main/java/com/cauergarcia/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.cauergarcia.workshopmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,4 +29,8 @@ public class PostService {
 		return postRepository.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
 }
